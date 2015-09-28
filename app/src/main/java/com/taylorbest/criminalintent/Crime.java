2421c0f23be875ad90408c7b2ca7638f1f5c5769
chrishad95 <chrishadley@gmail.com>
2015-09-28 13:55:10 -0400

    up up and away

---
+++ app/src/main/java/com/taylorbest/criminalintent/Crime.java
@@ -0,0 +1,20 @@
+package com.taylorbest.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by chadley on 9/28/2015.
+ */
+
+
+public class Crime {
+
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+
+    }
+}
