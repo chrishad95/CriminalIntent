2f41a22a4f5d1d683171e6ef49076d5ce94a19dc
chrishad95 <chrishadley@gmail.com>
2015-09-28 22:44:26 -0400

    working on the crime list

--- app/src/main/java/com/taylorbest/criminalintent/Crime.java
+++ app/src/main/java/com/taylorbest/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.taylorbest.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,13 +9,45 @@ import java.util.UUID;
 
 
 public class Crime {
-
-
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public UUID getId() {
+
+        return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getTitle() {
+        return mTitle;
+
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
 
     }
 }
---
+++ app/src/main/java/com/taylorbest/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.taylorbest.criminalintent;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by chadley on 9/28/2015.
+ */
+public class CrimeListFragment extends ListFragment {
+
+}
